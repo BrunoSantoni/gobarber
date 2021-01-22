6ee0e02ae53c05e3c178eab26d79e24726c2f0e2
BrunoSantoni <brunosantoni98@gmail.com>
2021-01-21 21:44:04 -0300

    feat: tests on appointments service

---
+++ server/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,41 @@
+import AppError from '@shared/errors/AppError';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.run({
+      date: new Date(),
+      providerId: '123456789',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.providerId).toBe('123456789');
+  });
+
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2021, 0, 21, 21);
+
+    await createAppointment.run({
+      date: appointmentDate,
+      providerId: '123123',
+    });
+
+    expect(
+      createAppointment.run({
+        date: appointmentDate,
+        providerId: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
